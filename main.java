8ddceb0ec1a95b9fa4057f22f8122cb9d2d7ca5c
Battula Praveenkumar <64602251+Battulapraveenkumar@users.noreply.github.com>
2021-08-16 23:34:11 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,25 @@
+public class Arithmeticoperations {
+    int num1,num2;
+    Arithmeticoperations(int num1, int num2)
+    {
+        this.num1=num1;
+        this.num2=num2;
+    }
+    public int sum()
+    {
+        return num1+num2;
+    }
+
+    public int sub() {
+        return num1-num2;
+    }
+
+    public int multiply() {
+        return num1*num2;
+    }
+
+    public int  division() {
+        return num1%num2;
+    }
+
+}
